e93b99a661c451380c8960f2bd263b472df166dc
Jscaldwell55 <jay.s.caldwell@gmail.com>
2025-02-13 19:02:16 -0600

    no

--- frontend/src/components/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen.tsx
@@ -1,3 +1,4 @@
+// src/components/LoadingScreen.tsx
 import React from 'react';
 
 const LoadingScreen: React.FC = () => {
@@ -10,7 +11,7 @@ const LoadingScreen: React.FC = () => {
           style={{ width: '100%' }} // Animate to 100% width
         />
       </div>
-      <p className="absolute top-4 right-4 text-sm text-gray-500">By Jay</p>
+      {/* REMOVE THIS LINE */}
     </div>
   );
 };
